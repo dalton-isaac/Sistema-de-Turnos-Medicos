a21038d77b4a286b66d9de9e8517df578fa6e474
Dalton Isaac <lam_237@outlook.com>
2026-06-30 11:59:24 -0500

    Se implemento la cola (Queue) usando LinkedList

---
+++ src/SistemaCentroSalud.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SistemaCentroSalud{
+
+    public static void main(String [] args){
+
+        //Imprementacion de la Cola (Queue) para almacenar los pacientes
+        Queue<Paciente> colaEspera = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0 ;
+        System.out.println("=====SISTEMA DE REGISTRO DEL CENTRO DE SALUD=====");
+
+        
+    }
+}
