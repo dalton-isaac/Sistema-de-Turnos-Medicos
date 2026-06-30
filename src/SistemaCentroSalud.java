e6c3b63b99059d1a17f88d0ea5789e0dcedba71d
Dalton Isaac <lam_237@outlook.com>
2026-06-30 12:08:32 -0500

    Se realizó las opciones del menu

--- src/SistemaCentroSalud.java
+++ src/SistemaCentroSalud.java
@@ -8,8 +8,21 @@ public class SistemaCentroSalud{
         Queue<Paciente> colaEspera = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
         int opcion = 0 ;
-        System.out.println("=====SISTEMA DE REGISTRO DEL CENTRO DE SALUD=====");
 
+        // Menu de opciones para el usuario
+        System.out.println("=====SISTEMA DE REGISTRO DEL CENTRO DE SALUD=====");
+        
+        do{
+            System.out.println("\n---Menú de opciones---");
+            System.out.println("1. Registrar nuevo paciente");
+            System.out.println("2. Atender siguiente paciente");
+            System.out.println("3. Ver número de pacientes en espera");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese una opción: ");
+
+
+            
+        }
         
     }
 }
