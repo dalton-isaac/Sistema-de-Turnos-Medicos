4e6c802bd4f65f9ac2ab6ff769b1e123f6ac74cd
Dalton Isaac <lam_237@outlook.com>
2026-06-30 00:05:42 -0500

    Se crea la clase que modela la entidad Paciente y su Constructor

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,3 +1,13 @@
+//Clase que modela la entidad Paciente
 public class Paciente {
-    
+    private String nombre;
+    private int edad;
+    private String motivoConsulta;
+
+    // Constructor
+    public Paciente(String nombre, int edad, String motivoConsulta){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.motivoConsulta = motivoConsulta;
+    }
 }
